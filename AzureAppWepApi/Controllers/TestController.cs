f825b5819319c324757824dba79a7dbb98872f14
ritesh.kumar80037 <ritesh.kumar80037@gmail.com>
2024-12-27 22:04:09 +0530

    test controller

---
+++ AzureAppWepApi/Controllers/TestController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AzureAppWepApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        List<Student> students;
+        public TestController()
+        {
+            students = new List<Student>();
+        }
+        [HttpGet]
+        public IActionResult GetStudnetData()
+        {
+
+            students.Add(new Student
+            {
+                Id = 1,
+                Class = "10th",
+                Name = "Ritesh"
+
+            });
+            students.Add(new Student
+            {
+                Id = 2,
+                Class = "12th",
+                Name = "Priyanka"
+
+            });
+            return Ok(students);
+        }
+    }
+}

4726a500ff99815c732d562b88f38c841ce0faf9
ritesh.kumar1 <ritesh.kumar1@RITESH-SFIN787>
2024-12-27 21:50:34 +0530

    Add project files.

---
+++ AzureAppWepApi/Controllers/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace AzureAppWepApi.Controllers
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Class { get; set; }
+
+    }
+}
